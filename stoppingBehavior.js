5ead5cc5b703c3dcf984de7f2f276bc43dce970a
Donna Quach <donnaq8@uw.edu>
2023-11-21 15:30:17 -0800

    Files needed for Exercise 6

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,3 +1,6 @@
+// Donna Quach, JavaScript 310B, Autumn 2023
+// Class 6 Exercises 
+
 // Do not change
 document.getElementById('cat').addEventListener('click', () => {
   alert('meow!');
@@ -8,3 +11,32 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+
+$(document).ready(function() {
+  // When clicked, "More info" link should alert "Here's some info"
+  // instead of going to a new webpage
+  const $moreInfoLink = $('#more-info'); // jQuery object with element with id="more-info"
+  $($moreInfoLink).click(function() {
+    alert('Here\'s some info'); 
+  }); 
+
+
+  // When the bark button is clicked, should alert "Bow wow!"
+  // Should *not* alert "meow"
+  const $dogButton = $('#dog'); // jQuery object with element with id="dog"
+
+  $($dogButton).click(function() {
+    alert('Bow wow!');
+  });
+
+}); 
+
+  
+ 
+
+
+
+
+
+
